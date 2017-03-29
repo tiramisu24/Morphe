c0e7a6d236a384f9538ec3050816960b71a677aa
App Academy Student <student@appacademy.io>
2017-03-29 10:38:18 -0700

    Refactor eyebrows. Draw left eyeliner

--- js/features/draw_eyebrows.js
+++ js/features/draw_eyebrows.js
@@ -1,22 +1,20 @@
-const drawEyebrows = (pos1, pos2) => {
-  drawLeftBrow(pos1);
-}
-
-
-const drawLeftBrow = (pos,canvas) => {
+const drawEyebrows = (pos1, pos2, canvas) => {
+  drawBrow(pos1, canvas ,14, 6);
+  drawBrow(pos2, canvas , 14, -16);
+};
+const drawBrow = (pos,canvas, yOffset, xOffset) => {
   const ctx = canvas.getContext('2d');
 
-  let x1 = pos[0][0];
-  let y1 = pos[0][1];
-  let x2 = pos[1][0];
-  let y2 = pos[1][1];
-  let x3 = pos[2][0];
-  let y3 = pos[2][1];
-  let x4 = pos[3][0];
-  let y4 = pos[3][1];
-  let yOffset = 14;
+  const x1 = pos[0][0];
+  const y1 = pos[0][1];
+  const x2 = pos[1][0];
+  const y2 = pos[1][1];
+  const x3 = pos[2][0];
+  const y3 = pos[2][1];
+  const x4 = pos[3][0];
+  const y4 = pos[3][1];
 
-  ctx.beginPath();
+
   const length = Math.sqrt(Math.pow((x3-x2),2) + Math.pow((y3-y2),2));
   const x = x2 + (length/4);
   const y = y2 - (Math.sqrt(3)/2 * length/4);
@@ -25,48 +23,15 @@ const drawLeftBrow = (pos,canvas) => {
   ctx.beginPath();
  //4
   ctx.moveTo(x4, y4); //4
-  ctx.lineTo( x4 + 6, y4-yOffset); //3
+  ctx.lineTo( x4 + xOffset, y4-yOffset); //3
   ctx.lineTo( x3, y3-yOffset); //3
   ctx.quadraticCurveTo(x,y, x2, y2-yOffset); //2
   ctx.lineTo(x1, y1 ); //1
   ctx.lineTo( (x3+x2*2)/3, (y3+y2)/2);
   ctx.lineTo( (x3*2+x2)/3, (y3+y2)/2);
   ctx.lineTo( x2, y2-yOffset); //2
-  ctx.lineTo( x4-6, y4+yOffset);
+  ctx.lineTo( x4-xOffset, y4+yOffset);
+    ctx.closePath();
 
-  ctx.closePath();
   ctx.fill();
-}
-
-const drawRightBrow = (pos,canvas) => {
-  const ctx = canvas.getContext('2d');
-  console.log("In right brow");
-  let x1 = pos[0][0];
-  let y1 = pos[0][1];
-  let x2 = pos[1][0];
-  let y2 = pos[1][1];
-  let x3 = pos[2][0];
-  let y3 = pos[2][1];
-  let x4 = pos[3][0];
-  let y4 = pos[3][1];
-  let yOffset = 14;
-
-  ctx.beginPath();
-  const length = Math.sqrt(Math.pow((x3-x2),2) + Math.pow((y3-y2),2));
-  const x = x2 - (length/4);
-  const y = y2 - (Math.sqrt(3)/2 * length/4);
-  // Cubic curves example
-  ctx.beginPath();
-
-  ctx.moveTo(x4, y4); //4
-  ctx.lineTo( x4 - 6, y4-yOffset); //3
-  ctx.lineTo( x3, y3-yOffset); //3
-  ctx.quadraticCurveTo(x,y, x2, y2-yOffset); //2
-  ctx.lineTo(x1, y1); //1
-  ctx.lineTo( (x3+x2)/2, (y3+y2)/2);
-  ctx.lineTo( x2, y2 - yOffset); //2
-  ctx.lineTo( x4+6, y4+yOffset);
-
-  ctx.closePath();
-  ctx.fill();
-}
+};
--- js/features/draw_eyeliner.js
+++ js/features/draw_eyeliner.js
@@ -1,4 +1,42 @@
 const drawRightEyeliner = (pos,canvas) => {
-  
+  const x1 = pos[2][0];
+  const y1 = pos[2][1];
+  const x2 = pos[5][0];
+  const y2 = pos[5][1];
+  const x3 = pos[1][0];
+  const y3 = pos[1][1];
+  const x4 = pos[4][0];
+  const y4 = pos[4][1];
+  const x5 = pos[0][0];
+  const y5 = pos[0][1];
+  const eyebrowx = pos[8][0];
+  const eyebrowy = pos[8][1];
 
-}
+  const yOffset = 6;
+  let y6,y7,y8,y9,y10;
+  y6 = y5 - yOffset;
+  y7 = y4 - yOffset;
+  y8 = y3 - yOffset;
+  y9 = y2 - yOffset;
+  y10 = y1 - yOffset;
+
+  let bx = (eyebrowx-x5)/3 + x5;
+  let by = (eyebrowy-y4)/4 + y5;
+  const ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.lineTo(x3, y3);
+  ctx.lineTo(x4, y4);
+  ctx.lineTo(x5, y5);
+  ctx.lineTo(bx, by);
+  // ctx.lineTo(x5, y6);
+  ctx.lineTo(x4, y7);
+  ctx.lineTo(x3, y8);
+  ctx.lineTo(x2, y9);
+  ctx.lineTo(x1, y10);
+
+  ctx.closePath();
+  ctx.fill();
+
+};
